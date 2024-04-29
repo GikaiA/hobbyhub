eea856ec4fdf577ff89e53913f7818759e4a88cb
Gikai Andrews <gnaboys@gmail.com>
2024-04-28 23:33:12 -0400

    did changes

---
+++ build/static/js/952.89f16fe4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkhobbyhub=self.webpackChunkhobbyhub||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.89f16fe4.chunk.js.map
